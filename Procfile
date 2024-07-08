0b5d60555a1b9ac2c4d8409d15800bb3b344685c
HelloCigar? <jckarim@up.edu.ph>
2024-07-08 15:39:47 +0800

    add config for Choreo

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn job_posting_backend.wsgi --log-file -
+web: python manage.py runserver 0.0.0.0:8000
--- job_posting_backend/settings.py
+++ job_posting_backend/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,6 +27,7 @@ SECRET_KEY = 'django-insecure-11ro^4rv2#lmjjesj$x93ao7iwyd$yedc_wo5&w7zt+zkz53p$
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 
 # Application definition
@@ -100,8 +103,12 @@ WSGI_APPLICATION = 'job_posting_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PWD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
